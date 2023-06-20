a60f3d20ddb82f6f579587f449f084663aa28c04
강다솔 <106011096+Dasolss@users.noreply.github.com>
2023-06-20 17:05:06 +0900

    Add files via upload

---
+++ AI/KT_API_GenieMemo/TestCommon/conf.py
@@ -0,0 +1,15 @@
+config =\
+    {
+        'api':
+        {
+            'clientID':'clientID input',
+            'apiKey':'apiKey input',
+            'secret':'secret input',
+        }
+    }
+
+class bcolors:
+    HEADER = '\033[94m'
+    WARNING = '\033[93m'
+    ERROR = '\033[91m'
+    ENDC = '\033[0m'
